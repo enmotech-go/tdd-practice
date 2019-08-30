977207f235dd25c3b16e5b29734b7fc62bfbbd91
Rory <hui.luo@enmotech.com>
2019-08-30 19:01:30 +0800

    refactory testing case for saying'Hello, world'

--- class01/hello/hello_test.go
+++ class01/hello/hello_test.go
@@ -3,10 +3,12 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	t.Run("Say hello world as default testing case", func(t *testing.T) {
+		got := Hello()
+		want := "Hello, world"
 
-	if got != want {
-		t.Errorf("want '%s' got '%s' ", want, got)
-	}
+		if got != want {
+			t.Errorf("want '%s' got '%s' ", want, got)
+		}
+	})
 }
