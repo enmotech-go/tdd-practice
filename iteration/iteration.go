3a110e358700712a517cebf9c36b78ab18b77f3c
chenming <ming.chen@enmotech.com>
2019-08-30 18:28:23 +0800

    加入基准测试，迭代代码

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,5 +1,11 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(character string) string {
-	return ""
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,10 +3,16 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("str")
-	expected := "str__"
+	repeated := Repeat("a")
+	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected '%s' but got '%s'", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
 	}
 }
