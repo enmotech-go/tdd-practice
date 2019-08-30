39e68e976b319d613b6303eb37a9d8bf94dbf8f0
daiwei <wei.dai@enmotech.com>
2019-08-30 18:11:10 +0800

    重构repeat

--- repeat/repeat.go
+++ repeat/repeat.go
@@ -1,8 +1,10 @@
 package repeat
 
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated = repeated + character
 	}
 	return repeated
