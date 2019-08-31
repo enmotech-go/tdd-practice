04843885a348090b57b28d91c63c86071eaecab7
Rory <hui.luo@enmotech.com>
2019-08-31 08:55:00 +0800

    refactory test case by sub-test

--- class01/hello/hello_test.go
+++ class01/hello/hello_test.go
@@ -3,21 +3,23 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, want, got string) {
+		if got != want {
+			t.Errorf("want '%s' got '%s' ", want, got)
+		}
+	}
+
 	t.Run("Say hello world as default testing case", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
 
-		if got != want {
-			t.Errorf("want '%s' got '%s' ", want, got)
-		}
+		assertCorrectMessage(t, want, got)
 	})
 
 	t.Run("Say hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("want '%s' got '%s' ", want, got)
-		}
+		assertCorrectMessage(t, want, got)
 	})
 }
