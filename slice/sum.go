220b17c7a84b8627109952cddfd9d279c06b81d4
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-09-02 17:53:19 +0800

    range重构

--- slice/sum.go
+++ slice/sum.go
@@ -2,8 +2,8 @@ package main
 
 func Sum(numbers [5]int) (ret int) {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
