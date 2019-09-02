fd2beb443c2ef4a6697cc5ffacb5b947f2b921c4
hongwei.wu <hongwei.wu@enmotech.com>
2019-09-02 18:38:46 +0800

    refine SumAll

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -8,14 +8,11 @@ func Sum(numbers []int)int{
 	return sum
 }
 
-
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
-	return
+	return sums
 }
