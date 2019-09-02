de1c1d1045e8676337965556926706aebbdc69e4
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-09-02 19:22:38 +0800

    重构测试用例

--- slice/sum_test.go
+++ slice/sum_test.go
@@ -20,7 +20,6 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAll(t *testing.T) {
-
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
@@ -47,6 +46,28 @@ func TestSumAllTails(t *testing.T) {
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+	})
+
+}
+
+func TestSumAllTails2(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
 	})
 
 }
