87e87ec64ef9b58791def3c193209be3eb5fa9aa
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-09-02 18:03:01 +0800

    重构

--- slice/sum.go
+++ slice/sum.go
@@ -7,13 +7,11 @@ func Sum(numbers []int) (sum int) {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
-	return
+	return sums
 }
