479aee93c8232935cb9d0c83c0fe929c028eaefd
hongwei.wu <hongwei.wu@enmotech.com>
2019-09-02 18:27:03 +0800

    implement SumAll

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -10,5 +10,12 @@ func Sum(numbers []int)int{
 
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
 	return
 }
