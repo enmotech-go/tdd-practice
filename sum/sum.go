df350d7e80708a3d5bf1650539c64423cb06b8e6
daiwei <wei.dai@enmotech.com>
2019-09-02 19:21:16 +0800

    添加SumAllTails

--- sum/sum.go
+++ sum/sum.go
@@ -14,3 +14,13 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 
 	return
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
