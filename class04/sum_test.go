3a05dda56fff206eb19336e6181309ca77594a93
樊建 <jian.fan@enmotech.com>
2019-09-02 18:40:03 +0800

    add sum all unit test case

--- class04/sum_test.go
+++ class04/sum_test.go
@@ -16,3 +16,12 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{3, 4})
+	want := []int{3, 7}
+
+	if got != want {
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
+}
