840bbba83b5bb5d236fb4a1d7ff9bb9634e84209
Rory <hui.luo@enmotech.com>
2019-09-02 19:13:54 +0800

    implemented the function 'SumAll'

--- class03/sum.go
+++ class03/sum.go
@@ -9,3 +9,13 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
--- class03/sum_test.go
+++ class03/sum_test.go
@@ -1,6 +1,9 @@
 package class03
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -13,4 +16,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d", got, want)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
