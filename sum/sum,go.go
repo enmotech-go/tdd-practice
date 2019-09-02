7dfb0a99e9d0124bd9e1beb832309df5032fedf4
yq <qin.yin@enmotech.com>
2019-09-02 19:11:23 +0800

    重构sumall使用append方法

--- sum/sum,go.go
+++ sum/sum,go.go
@@ -8,12 +8,9 @@ func Sum(numbers []int) (sum int) {
 }
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
 
-	sums = make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 	return
 }
