040d1cc97352dab29852caba22444c9c91be4ddd
liuyang <yang.liu01@enmotech.com>
2019-09-02 19:37:36 +0800

    实现面积的计算

--- class5/shapes.go
+++ class5/shapes.go
@@ -3,3 +3,8 @@ package class5
 func Perimeter(w, h float64) float64 {
 	return 2 * (w + h)
 }
+
+//noinspection GoUnusedCallResult
+func Area(w, h float64) float64 {
+	return w * h
+}
--- class5/shapes_test.go
+++ class5/shapes_test.go
@@ -9,3 +9,11 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("got '%.2f' want '%.2f'", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	want := 72.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
