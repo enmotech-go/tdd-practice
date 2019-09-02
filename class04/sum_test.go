fdc39f3e8f26f6bf19c19f2f9a7dd8f1e7f39616
樊建 <jian.fan@enmotech.com>
2019-09-02 18:26:40 +0800

    make sum function correct

--- class04/sum_test.go
+++ class04/sum_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
