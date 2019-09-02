a4e6b1c8f81e4bc2fe037ef9f8590f8b5066de3d
樊建 <jian.fan@enmotech.com>
2019-09-02 18:24:05 +0800

    construct sum function

--- class04/sum.go
+++ class04/sum.go
@@ -1,9 +1,8 @@
 package class04
 
-func Sum(numbers [5]int) int {
-	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+func Sum(numbers [5]int) (sum int) {
+	for _, number := range numbers {
+		sum += number
 	}
-	return sum
+	return
 }
