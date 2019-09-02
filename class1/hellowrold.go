8d05742ed0f59ba81963c307baa93b60bf509156
liuyang <yang.liu01@enmotech.com>
2019-09-02 09:02:50 +0800

    入参没有默认返回hello world

--- class1/hellowrold.go
+++ class1/hellowrold.go
@@ -3,5 +3,8 @@ package class1
 const helloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
 	return helloPrefix + name
 }
--- class1/hellowrold_test.go
+++ class1/hellowrold_test.go
@@ -3,9 +3,19 @@ package class1
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got '%s' ,want '%s'", got, want)
-	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		if got != want {
+			t.Errorf("got '%s' ,want '%s'", got, want)
+		}
+	})
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		if got != want {
+			t.Errorf("got '%s' ,want '%s'", got, want)
+		}
+	})
 }
