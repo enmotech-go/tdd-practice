438e55bd7851ccfc932fd1e29ef9da782c9330cb
yq <qin.yin@enmotech.com>
2019-09-02 19:27:59 +0800

    SumAllTails函数实现

--- sum/sum,go.go
+++ sum/sum,go.go
@@ -8,9 +8,16 @@ func Sum(numbers []int) (sum int) {
 }
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
-
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sum []int) {
+	for _, number := range numbersToSum {
+		tail := number[1:]
+		sum = append(sum, Sum(tail))
 	}
 	return
 }
