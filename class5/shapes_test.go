254587a0019754c9f5fca4882d5cf0157e743fc3
liuyang <yang.liu01@enmotech.com>
2019-09-02 19:30:44 +0800

    优化float64的输出

--- class5/shapes_test.go
+++ class5/shapes_test.go
@@ -6,6 +6,6 @@ func TestPerimeter(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
 	if got != want {
-		t.Errorf("got '%f' want '%f'", got, want)
+		t.Errorf("got '%.2f' want '%.2f'", got, want)
 	}
 }
