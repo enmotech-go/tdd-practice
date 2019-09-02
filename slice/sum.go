f7404a908b54c66e0da376c753ad10dea0461e7c
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-09-02 18:23:03 +0800

    测试通过

--- slice/sum.go
+++ slice/sum.go
@@ -7,10 +7,11 @@ func Sum(numbers []int) (sum int) {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) []int {
+func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers))
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
 	}
 
 	return sums
--- slice/sum_test.go
+++ slice/sum_test.go
@@ -19,9 +19,9 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func TestSumAll(t *testing.T) {
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
