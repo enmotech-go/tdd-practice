ffa35c2f3e7012c08046f3c12bdf050b3c2edfb6
liuyang <yang.liu01@enmotech.com>
2019-09-02 09:10:21 +0800

    实现Spanish的打招呼方式

--- class1/hellowrold.go
+++ class1/hellowrold.go
@@ -1,10 +1,14 @@
 package class1
 
 const helloPrefix = "Hello, "
+const PrefixSpanish = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
+	}
+	if language == "Spanish" {
+		return PrefixSpanish + name
 	}
 	return helloPrefix + name
 }
--- class1/hellowrold_test.go
+++ class1/hellowrold_test.go
@@ -12,14 +12,21 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 		assertCorrectMessage(t, got, want)
 
 	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+
+	})
+
 }
