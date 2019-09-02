c4992f7c28dbe7bcbcee3311d661c4fc303bb16f
樊建 <jian.fan@enmotech.com>
2019-09-02 19:24:37 +0800

    add sum all tails safely unit test and make it correct

--- class04/sum.go
+++ class04/sum.go
@@ -16,8 +16,12 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 
 func SumAllTails(numbersToSum ...[]int) (sums []int) {
 	for _, numbers := range numbersToSum {
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return
 }
