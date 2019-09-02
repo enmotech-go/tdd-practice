cc2a605a0d17fd4d65d733280814b71b34b53ba7
liuyang <yang.liu01@enmotech.com>
2019-09-02 09:05:32 +0800

    修改测试用例，加入t.Helper

--- class1/hellowrold_test.go
+++ class1/hellowrold_test.go
@@ -4,18 +4,22 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got '%s' ,want '%s'", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
-		if got != want {
-			t.Errorf("got '%s' ,want '%s'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
+
 	})
 }
