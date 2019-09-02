584ba03105d1fb8f50f10721801738d612c14ded
樊建 <jian.fan@enmotech.com>
2019-09-02 18:37:35 +0800

    remove first unit test case

--- class04/sum_test.go
+++ class04/sum_test.go
@@ -5,17 +5,6 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
-		got := Sum(numbers)
-		want := 15
-
-		if want != got {
-			t.Errorf("got '%d' want '%d' given, '%v'", got, want, numbers)
-		}
-	})
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5, 6}
 
