d79225d4413796489cae02a49b35af5c53dc046c
yq <qin.yin@enmotech.com>
2019-09-04 17:57:09 +0800

    增加求长方形面积的需求

--- premeter/premeter.go
+++ premeter/premeter.go
@@ -3,3 +3,7 @@ package premeter
 func Permeter(width, height float64) float64 {
 	return (width + height) * 2
 }
+
+func Area(width, height float64) float64 {
+	return width * height
+}
--- premeter/premeter_test.go
+++ premeter/premeter_test.go
@@ -4,11 +4,18 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	got := Permeter(5.0, 10.0)
-
 	want := 30.0
 
 	if got != want {
 		t.Errorf("want %.2f, got %.2f", want, got)
 	}
+}
 
+func TestArea(t *testing.T) {
+	got := Area(12.0, 5.0)
+	want := 60.0
+
+	if got != want {
+		t.Errorf("want %.2f, got %.2f", want, got)
+	}
 }
