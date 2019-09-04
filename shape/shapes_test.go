095122651ad9cfc4d76c3edc11872c2892685aab
daiwei <wei.dai@enmotech.com>
2019-09-04 18:30:31 +0800

    添加struct 保证单元测试通过

--- shape/shapes_test.go
+++ shape/shapes_test.go
@@ -14,11 +14,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		t.Log("got = ", got)
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %f want %f", got, want)
+		}
+	})
+
 }
