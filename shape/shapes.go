a6ad5b4540719fd6c1a7c2045e571a6f2a0c7aec
daiwei <wei.dai@enmotech.com>
2019-09-04 18:03:54 +0800

    添加area函数并保证单元测试通过

--- shape/shapes.go
+++ shape/shapes.go
@@ -3,3 +3,7 @@ package shape
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
+
+func Area(width float64, height float64) float64 {
+	return width * height
+}
