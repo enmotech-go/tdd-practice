c2f4698b8b302abac10d860f0ab872c6d5912e03
Rory <hui.luo@enmotech.com>
2019-09-04 18:20:01 +0800

    implemented the circle's area & perimeter

--- class04/shapes.go
+++ class04/shapes.go
@@ -1,5 +1,7 @@
 package class04
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -11,4 +13,16 @@ func (r Rectangle) Perimeter() float64 {
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (a Circle) Perimeter() float64 {
+	return 2 * math.Pi * a.Radius
+}
+
+func (a Circle) Area() float64 {
+	return math.Pi * a.Radius * a.Radius
 }
--- class04/shapes_test.go
+++ class04/shapes_test.go
@@ -1,6 +1,9 @@
 package class04
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{Width: 10.0, Height: 10.0}
@@ -13,11 +16,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{Width: 12.0, Height: 5.0}
-	got := rectangle.Area()
-	want := 60.0
+	t.Run("Rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{Width: 12.0, Height: 5.0}
+		got := rectangle.Area()
+		want := 60.0
 
-	if got != want {
-		t.Errorf("got %0.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %0.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("Circle area", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := math.Pi * 10 * 10
+
+		if got != want {
+			t.Errorf("got %0.2f want %.2f", got, want)
+		}
+	})
 }
