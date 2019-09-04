0763e61c9c0696078b54a903d46f1e3882e2671e
chenming <ming.chen@enmotech.com>
2019-09-04 17:57:06 +0800

    新增需求TDD提交

--- struct_tdd/shapes.go
+++ struct_tdd/shapes.go
@@ -1,7 +1,11 @@
 package struct_tdd
 
 //Perimeter 计算周长函数
-func Perimeter(width,height float64)(area float64){
-	area = 2*(width + height)
+func Perimeter(width,height float64)(perimeter float64){
+	perimeter = 2*(width + height)
 	return
+}
+
+func Area(width ,height float64) float64 {
+	return width * height
 }
