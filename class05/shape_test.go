7d0337862af7358eeaa77629f2754e420e5614d7
樊建 <jian.fan@enmotech.com>
2019-09-04 18:52:36 +0800

    construct area unit test function with name

--- class05/shape_test.go
+++ class05/shape_test.go
@@ -16,18 +16,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	tests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 8}, 96.0},
-		{Circle{10.0}, math.Pi * 10.0 * 10.0},
-		{Triangle{12.0, 6.0}, 36.0},
+		{"rectangle", Rectangle{12, 8}, 96.0},
+		{"circle", Circle{10.0}, math.Pi * 10.0 * 10.0},
+		{"triangle", Triangle{12.0, 6.0}, 36.0},
 	}
 
 	for _, tt := range tests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("Area() = %.2f, want %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("Area() = %.2f, want %.2f", got, tt.want)
+			}
+		})
 	}
 }
