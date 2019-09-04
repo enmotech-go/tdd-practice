bcb0dae10684fce70dcabbb42407c5d48dfbb64d
Rory <hui.luo@enmotech.com>
2019-09-04 18:55:59 +0800

    rectory the test case

--- class04/shapes_test.go
+++ class04/shapes_test.go
@@ -17,18 +17,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, math.Pi * 10 * 10},
-		{Triangle{10.0, 5.0}, 25.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, want: 72.0},
+		{name: "Circle", shape: Circle{10}, want: math.Pi * 10 * 10},
+		{name: "Triangle", shape: Triangle{10.0, 5.0}, want: 25.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %.2f want %.2f", got, tt.want)
+			}
+		})
 	}
 }
