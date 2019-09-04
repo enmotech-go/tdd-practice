fd56b679ada01d98cf4129177bd1b92e7cd4ded8
hongwei.wu <hongwei.wu@enmotech.com>
2019-09-04 17:52:31 +0800

    add func Area

--- struct_method_interface/struct_method_interface.go
+++ struct_method_interface/struct_method_interface.go
@@ -3,3 +3,7 @@ package struct_method_interface
 func Perimeter(width, height float64) float64 {
 	return (width + height) * 2
 }
+
+func Area(width, height float64) float64 {
+	return width * height
+}
--- struct_method_interface/struct_method_interface_test.go
+++ struct_method_interface/struct_method_interface_test.go
@@ -9,3 +9,11 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 10.0)
+	want := 100.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
