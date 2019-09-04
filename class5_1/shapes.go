481b71ad44c81e5d5adcb1e4fdb534ed3e28c437
liuyang <yang.liu01@enmotech.com>
2019-09-04 17:54:11 +0800

    实现周长计算

---
+++ class5_1/shapes.go
@@ -0,0 +1,5 @@
+package class5_1
+
+func Perimeter(w, h float64) float64 {
+	return 2 * (w + h)
+}
---
+++ class5_1/shapes_test.go
@@ -0,0 +1,13 @@
+package class5_1
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(12.0, 5.0)
+	want := 34.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+
+	}
+
+}
