ab39f2710c7bc10c1ebc7e2cf1f9bb2a78733c0a
kliyes <tom@kliyes.com>
2019-09-04 19:22:21 +0800

    table driven test

--- structure/method_test.go
+++ structure/method_test.go
@@ -44,3 +44,20 @@ func TestArea3(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793)
 	})
 }
+
+func TestArea4(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+}
