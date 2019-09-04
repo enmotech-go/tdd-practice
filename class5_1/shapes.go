8c08574a0a13ca11a846e0eb1dca1594c397fe54
liuyang <yang.liu01@enmotech.com>
2019-09-04 18:23:55 +0800

    实现圆的面积计算

--- class5_1/shapes.go
+++ class5_1/shapes.go
@@ -1,14 +1,24 @@
 package class5_1
 
+import "math"
+
 type Rectangle struct {
 	W float64
 	H float64
+}
+
+type Circle struct {
+	R float64
 }
 
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.W + r.H)
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.H * r.W
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.R * c.R
 }
--- class5_1/shapes_test.go
+++ class5_1/shapes_test.go
@@ -1,6 +1,9 @@
 package class5_1
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{
@@ -15,13 +18,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{
-		W: 12.0,
-		H: 5.0,
-	}
-	got := Area(rectangle)
-	want := 60.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{
+			W: 12.0,
+			H: 5.0,
+		}
+		got := rectangle.Area()
+		want := 60.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := Circle.Area(circle)
+		want := math.Pi * 10.0 * 10.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
 }
