e54918e69292ab5c8de367757eb7cf7651d35b13
liuyang <yang.liu01@enmotech.com>
2019-09-04 21:49:54 +0800

    初步实现了钱包的功能

--- class6/wallet.go
+++ class6/wallet.go
@@ -1,12 +1,13 @@
 package class6
 
 type Wallet struct {
+	balance int
 }
 
-func (wallet Wallet) Deposit(i int) {
-
+func (wallet *Wallet) Deposit(amount int) {
+	wallet.balance += amount
 }
 
-func (wallet Wallet) Balance() int {
-	return 10
+func (wallet *Wallet) Balance() int {
+	return wallet.balance
 }
--- class6/wallet_test.go
+++ class6/wallet_test.go
@@ -1,10 +1,15 @@
 package class6
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
+
+	fmt.Println("address of balance in test is", &wallet.balance)
 	got := wallet.Balance()
 	want := 10
 	if got != want {
