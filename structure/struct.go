817981a52735bd5be3db8864a14e4c82cf1df4eb
kliyes <tom@kliyes.com>
2019-09-04 17:57:47 +0800

    add Area() function

--- structure/struct.go
+++ structure/struct.go
@@ -3,3 +3,7 @@ package structure
 func Premeter(width, height float64) float64 {
 	return (width + height) * 2
 }
+
+func Area(width, height float64) float64 {
+	return width * height
+}
--- structure/struct_test.go
+++ structure/struct_test.go
@@ -1,6 +1,8 @@
 package structure
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPremeter(t *testing.T) {
 	type args struct {
@@ -25,6 +27,34 @@ func TestPremeter(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Premeter(tt.args.width, tt.args.height); got != tt.want {
 				t.Errorf("Premeter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestArea(t *testing.T) {
+	type args struct {
+		width  float64
+		height float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			"test calculate area",
+			args{
+				width:  11.2,
+				height: 6.5,
+			},
+			72.8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Area(tt.args.width, tt.args.height); got != tt.want {
+				t.Errorf("Area() = %v, want %v", got, tt.want)
 			}
 		})
 	}
