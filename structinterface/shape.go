03a41a1058693b5a218ee217bd4b55358b0f3c8b
huang peng <peng.huang2@enmotech.com>
2019-09-04 18:30:40 +0800

    interface

--- structinterface/shape.go
+++ structinterface/shape.go
@@ -11,6 +11,10 @@ type Circle struct {
 	Radius float64
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func (r Rectangle) Perimeter() float64 {
 	return (r.Width + r.Length) * 2
 }
--- structinterface/shape_test.go
+++ structinterface/shape_test.go
@@ -15,21 +15,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 5.0}
-		got := rectangle.Area()
-		want := 60.0
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f, want %.2f", got, want)
 		}
+	}
+	t.Run("Rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 5.0}
+		want := 60.0
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("Circle area", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := circle.Area()
+
 		want := math.Pi * 10.0 * 10.0
-		if got != want {
-			t.Errorf("got %.2f, want %.2f", got, want)
-		}
+		checkArea(t, circle, want)
 	})
 }
