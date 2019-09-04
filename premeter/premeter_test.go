4879c89f29c42dd59c03ba2449737e977bd7a563
yq <qin.yin@enmotech.com>
2019-09-04 18:54:50 +0800

    给测试函数加上name,可以单独跑某一个测试用例

--- premeter/premeter_test.go
+++ premeter/premeter_test.go
@@ -18,19 +18,22 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{10.0, 5.0}, 50.0},
-		{Circle{10}, math.Pi * 10 * 10},
-		{Triangle{12.0, 6.0}, 36.0},
+		{name: "Rectangle", shape: Rectangle{10.0, 5.0}, want: 50.0},
+		{name: "Circle", shape: Circle{10}, want: math.Pi * 10 * 10},
+		{name: "Triangle", shape: Triangle{12.0, 6.0}, want: 36.0},
 	}
 
 	for _, ss := range areaTests {
-		got := ss.shape.Area()
-		if math.Abs(got-ss.want) > 0.00001 {
-			t.Errorf("%#v, want %.2f, got %.2f", ss.shape, ss.want, got)
-		}
-	}
 
+		t.Run(ss.name, func(t *testing.T) {
+			got := ss.shape.Area()
+			if math.Abs(got-ss.want) > 0.00001 {
+				t.Errorf("%#v, want %.2f, got %.2f", ss.shape, ss.want, got)
+			}
+		})
+	}
 }
