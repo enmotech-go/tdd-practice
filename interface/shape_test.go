301790a51552789d841106802d171f52df60e8ab
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-09-04 18:47:04 +0800

    表格驱动测试

--- interface/shape_test.go
+++ interface/shape_test.go
@@ -1,27 +1,112 @@
 package shape
 
 import (
+	"math"
 	"testing"
 )
 
-func TestArea(t *testing.T) {
-
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+func TestRectangle_Perimeter(t *testing.T) {
+	type fields struct {
+		Width  float64
+		Height float64
 	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		// TODO: Add test cases.
+		{name: "test for Rectangle Perimeter", fields: struct {
+			Width  float64
+			Height float64
+		}{Width: 10.0, Height: 20.0}, want: 60.0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := Rectangle{
+				Width:  tt.fields.Width,
+				Height: tt.fields.Height,
+			}
+			if got := r.Perimeter(); got != tt.want {
+				t.Errorf("Rectangle.Perimeter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
+func TestCircle_Perimeter(t *testing.T) {
+	type fields struct {
+		Radius float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		// TODO: Add test cases.
+		{name: "test for Circle Perimeter", fields: struct{ Radius float64 }{Radius: 10.0}, want: 2 * math.Pi * 10.0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Circle{
+				Radius: tt.fields.Radius,
+			}
+			if got := c.Perimeter(); got != tt.want {
+				t.Errorf("Circle.Perimeter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+func TestRectangle_Area(t *testing.T) {
+	type fields struct {
+		Width  float64
+		Height float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		// TODO: Add test cases.
+		{name: "test for Rectangle Perimeter", fields: struct {
+			Width  float64
+			Height float64
+		}{Width: 10.0, Height: 20.0}, want: 200.0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := Rectangle{
+				Width:  tt.fields.Width,
+				Height: tt.fields.Height,
+			}
+			if got := r.Area(); got != tt.want {
+				t.Errorf("Rectangle.Area() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
+func TestCircle_Area(t *testing.T) {
+	type fields struct {
+		Radius float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		// TODO: Add test cases.
+		{name: "test for Circle Perimeter", fields: struct{ Radius float64 }{Radius: 10.0}, want: math.Pi * 10.0 * 10.0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Circle{
+				Radius: tt.fields.Radius,
+			}
+			if got := c.Area(); got != tt.want {
+				t.Errorf("Circle.Area() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
