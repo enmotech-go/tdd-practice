9acd3fb2b809838614615637ab9b07769cc17a12
kliyes <tom@kliyes.com>
2019-09-04 19:28:49 +0800

    named test cases

--- structure/method.go
+++ structure/method.go
@@ -22,3 +22,16 @@ type Shape interface {
 	Perimeter() float64
 	Area() float64
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func (t Triangle) Perimeter() float64 {
+	return 0
+}
--- structure/method_test.go
+++ structure/method_test.go
@@ -47,17 +47,21 @@ func TestArea3(t *testing.T) {
 
 func TestArea4(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{name: "rectangle", shape: Rectangle{12, 6}, want: 72.0},
+		{name: "circle", shape: Circle{10}, want: 314.1592653589793},
+		{name: "triangle", shape: Triangle{12, 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %.2f want %.2f", got, tt.want)
+			}
+		})
 	}
 }
