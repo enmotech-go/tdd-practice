9821df1cf05998173a53b26d119e959ccb713942
kliyes <tom@kliyes.com>
2019-09-04 19:12:41 +0800

    add test case

--- structure/method_test.go
+++ structure/method_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestPerimeter(t *testing.T) {
+func TestPerimeter3(t *testing.T) {
 	t.Run("test calculate perimeter of rectangle", func(t *testing.T) {
 		rectangle := Rectangle{Width: 10.1, Height: 6.9}
 		got := rectangle.Perimeter()
@@ -23,6 +23,28 @@ func TestPerimeter(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %2.f, want %.2f", got, want)
+		}
+	})
+}
+
+func TestArea3(t *testing.T) {
+	t.Run("test calculate area of rectangle", func(t *testing.T) {
+		rectangle := Rectangle{Width: 11.2, Height: 6.5}
+		got := rectangle.Area()
+		want := 72.8
+
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("test calculate area of circle", func(t *testing.T) {
+		circle := Circle{Radius: 10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %f, want %f", got, want)
 		}
 	})
 }
