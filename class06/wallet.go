4daa6f9ed4e7fd228e999c40e996197fe71ff354
樊建 <jian.fan@enmotech.com>
2019-09-09 17:51:39 +0800

    make wallet deposit and balance function correct

--- class06/wallet.go
+++ class06/wallet.go
@@ -1,12 +1,13 @@
 package class06
 
 type Wallet struct {
+	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
-
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
-	return 0
+func (w *Wallet) Balance() int {
+	return w.balance
 }
