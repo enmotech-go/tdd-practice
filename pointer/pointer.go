11829a996669550afbd2e856ac5569070f75cdc7
chenming <ming.chen@enmotech.com>
2019-09-09 17:50:23 +0800

    重构结构体内改造成为比特币类型

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -2,19 +2,22 @@ package pointer
 
 import "fmt"
 
+
+type Bitcoin int
+
 //Wallet define wallet struct
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 
 //Deposit deposit bitcoin to wallet
-func (w *Wallet) Deposit(amount int){
+func (w *Wallet) Deposit(amount Bitcoin){
 	w.balance += amount
 }
 
 //Balance get bitcoin from wallet
-func (w *Wallet) Balance() (balance int){
+func (w *Wallet) Balance() (balance Bitcoin){
  	balance = w.balance
 	fmt.Println("address of balance in Deposit is", &w.balance)
 	return
--- pointer/pointer_test.go
+++ pointer/pointer_test.go
@@ -12,7 +12,7 @@ func TestWallet(t *testing.T){
 	wallet := &Wallet{}
 	wallet.Deposit(10)
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 	fmt.Println("address of balance in test is", &wallet.balance)
 	if got != want{
 		t.Errorf("got %d want %d",got ,want)
