107bea18fe6a2c4bcf076736e581bd55afdccd26
樊建 <jian.fan@enmotech.com>
2019-09-09 17:54:13 +0800

    construct wallet balance

--- class06/wallet_test.go
+++ class06/wallet_test.go
@@ -7,10 +7,10 @@ import (
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
 		t.Errorf("Balance() = %v, want %v", got, want)
