c6b2c84531d660092d0c3d07e2d41cc3eed5a734
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-09-09 18:00:52 +0800

    测试通过

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -13,3 +13,6 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
--- pointer/pointer_test.go
+++ pointer/pointer_test.go
@@ -6,15 +6,32 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
+		got := wallet.Balance()
 
-	want := Bitcoin(10)
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(10)
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
 }
