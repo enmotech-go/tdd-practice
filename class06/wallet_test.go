46c36aeb295da62a0142e0ebdbf8c24f5ebd732e
樊建 <jian.fan@enmotech.com>
2019-09-09 18:09:12 +0800

    construct wallet unit test

--- class06/wallet_test.go
+++ class06/wallet_test.go
@@ -5,28 +5,21 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
 		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got.String() != want.String() {
+		if got != want {
 			t.Errorf("Balance() = '%v', want '%v'", got.String(), want.String())
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got.String() != want.String() {
-			t.Errorf("Balance() = '%v', want '%v'", got.String(), want.String())
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
