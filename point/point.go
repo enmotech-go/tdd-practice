f5737c96569add5d2af9dc757fc0ce4f658d30d8
daiwei <wei.dai@enmotech.com>
2019-09-09 17:57:37 +0800

    添加Bitcoin类型，单元测试通过

--- point/point.go
+++ point/point.go
@@ -1,13 +1,15 @@
 package point
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- point/point_test.go
+++ point/point_test.go
@@ -6,7 +6,7 @@ func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 	if got != want {
 		t.Errorf("got %d  want %d", got, want)
 	}
