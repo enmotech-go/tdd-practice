77b86365530b2e7cdd00912d1e6c60495fd30c6e
liuyang <yang.liu01@enmotech.com>
2019-09-09 18:53:56 +0800

    将匿名方法实现成函数

--- class6_1/wallet_test.go
+++ class6_1/wallet_test.go
@@ -3,23 +3,6 @@ package class6_1
 import "testing"
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t *testing.T, wallet *Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.GetBalance()
-		if got != want {
-			t.Errorf("got %s want %s ", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, got error, want string) {
-		if got == nil {
-			t.Errorf("wanted an error but didnt get one")
-		}
-
-		if got.Error() != want {
-			t.Errorf("got '%s', want '%s'", got, want)
-		}
-	}
 
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
@@ -30,9 +13,19 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
-		wallet.Withdraw(10)
+		_ = wallet.Withdraw(10)
 		want := Bitcoin(10)
 		assertBalance(t, &wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		_ = wallet.Withdraw(100)
+		want := Bitcoin(10)
+		got := wallet.GetBalance()
+		if got != want {
+			t.Log(got, want)
+		}
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -41,6 +34,23 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, &wallet, startingBalance)
-		assertError(t, err, InsufficientFundsError.Error())
+		assertError(t, err, InsufficientFundsError)
 	})
+}
+
+func assertError(t *testing.T, err error, want error) {
+	if err == nil {
+		t.Errorf("wanted an error but didnt get one")
+	}
+	if err != want {
+		t.Errorf("got '%s', want '%s'", err, want)
+	}
+}
+
+func assertBalance(t *testing.T, wallet *Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.GetBalance()
+	if got != want {
+		t.Errorf("got %s want %s ", got, want)
+	}
 }
