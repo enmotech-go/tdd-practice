547e788a3edb971eb3b584fc04383037fbe08fdd
huang peng <peng.huang2@enmotech.com>
2019-09-09 19:03:16 +0800

    errror 3

--- pointerError/wallet_test.go
+++ pointerError/wallet_test.go
@@ -5,12 +5,6 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	assert := func(t *testing.T, got, want Bitcoin) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %s, but want %s", got, want)
-		}
-	}
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -26,18 +20,27 @@ func TestWallet(t *testing.T) {
 		want := Bitcoin(10)
 		assert(t, got, want)
 	})
-	assertErr := func(t *testing.T, err error, want string) {
-		if err == nil {
-			t.Fatalf("want an error but do not get one")
-		}
-		if err.Error() != want {
-			t.Errorf("got %s, want %s", err, want)
-		}
-	}
+
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
 		w := Wallet{Bitcoin(20)}
 		err := w.Withdraw(100)
 		assert(t, w.Balance(), Bitcoin(20))
 		assertErr(t, err, "cannot withdraw, insufficient funds")
 	})
+}
+
+func assert(t *testing.T, got, want Bitcoin) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %s, but want %s", got, want)
+	}
+}
+
+func assertErr(t *testing.T, err error, want string) {
+	if err == nil {
+		t.Fatalf("want an error but do not get one")
+	}
+	if err.Error() != want {
+		t.Errorf("got %s, want %s", err, want)
+	}
 }
