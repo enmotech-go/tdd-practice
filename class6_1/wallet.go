31547637e17d347a7cc0142e74b6725c86f9ce85
liuyang <yang.liu01@enmotech.com>
2019-09-09 18:11:32 +0800

    实现花钱

--- class6_1/wallet.go
+++ class6_1/wallet.go
@@ -1,15 +1,31 @@
 package class6_1
 
+import "fmt"
+
 type Bitcoin int
 
 type Wallet struct {
 	balance Bitcoin
 }
 
+type Stringer interface {
+	String() string
+}
+
+//Deposit Deposit
 func (wallet *Wallet) Deposit(amount Bitcoin) {
 	wallet.balance += amount
 }
 
+//GetBalance GetBalance
 func (wallet *Wallet) GetBalance() Bitcoin {
 	return wallet.balance
+}
+
+func (wallet *Wallet) Withdraw(amount Bitcoin) {
+	wallet.balance -= amount
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- class6_1/wallet_test.go
+++ class6_1/wallet_test.go
@@ -3,11 +3,24 @@ package class6_1
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
-	got := wallet.GetBalance()
-	want := Bitcoin(10)
-	if got != want {
-		t.Errorf(" got %d want %d", got, want)
+	assertBalance := func(t *testing.T, wallet *Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.GetBalance()
+		if got != want {
+			t.Errorf("got %s want %s ", got, want)
+		}
 	}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, &wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		wallet.Withdraw(10)
+		want := Bitcoin(10)
+		assertBalance(t, &wallet, want)
+	})
 }
