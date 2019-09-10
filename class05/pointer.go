5da77bf9d9695f455e0ffcf5801cde8970762170
Rory <hui.luo@enmotech.com>
2019-09-10 08:38:51 +0800

    added function 'Withdraw'

--- class05/pointer.go
+++ class05/pointer.go
@@ -16,6 +16,10 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
--- class05/pointer_test.go
+++ class05/pointer_test.go
@@ -3,13 +3,28 @@ package class05
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := &Wallet{}
-	wallet.Deposit(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := &Wallet{}
+		wallet.Deposit(10)
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(10)
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
 }
