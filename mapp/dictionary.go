c00fea88d4eba076ee92d5820b83c6d12ca71117
huang peng <peng.huang2@enmotech.com>
2019-09-11 17:50:43 +0800

    map self deinfine type

--- mapp/dictionary.go
+++ mapp/dictionary.go
@@ -1,5 +1,7 @@
 package mapp
 
-func Search(dictionary map[string]string, key string) string {
+type Dictionary map[string]string
+
+func (dictionary Dictionary) Search(key string) string {
 	return dictionary[key]
 }
--- mapp/dictionary_test.go
+++ mapp/dictionary_test.go
@@ -3,8 +3,8 @@ package mapp
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just test"}
-	got := Search(dictionary, "test")
+	dictionary := Dictionary{"test": "this is just test"}
+	got := dictionary.Search("test")
 	want := "this is just test"
 	assertStrings(t, got, want)
 }
