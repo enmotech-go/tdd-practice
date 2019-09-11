6fa0d1de95c263dfa4d6bd9259d52725f52cd8c7
daiwei <wei.dai@enmotech.com>
2019-09-11 17:45:43 +0800

    重构单元测试

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -8,7 +8,13 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got '%s' want '%s' given, '%s'", got, want, "test")
+		t.Errorf("got '%s' want '%s'", got, want)
 	}
 }
