55a40d626c92bfc95eca98243114921e102abe1e
kliyes <tom@kliyes.com>
2019-09-11 17:50:02 +0800

    refactory

--- maps/map.go
+++ maps/map.go
@@ -1,7 +1,13 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	value, exist := d[word]
+	if !exist {
+		return "", errors.New("not found")
+	}
+	return value, nil
 }
--- maps/map_test.go
+++ maps/map_test.go
@@ -5,9 +5,22 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is a test"}
 
-	got := dictionary.Search("test")
-	want := "this is a test"
-	assertStrings(t, got, want)
+	t.Run("test known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is a test"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("test unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "not found"
+
+		if err == nil {
+			t.Fatal("excepted an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
