28e062695df059f5cfeb4262fff512d2d2a6c285
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-09-11 17:59:49 +0800

    去掉魔术错误

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -4,10 +4,12 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil
