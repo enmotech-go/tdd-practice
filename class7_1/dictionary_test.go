6e061302985e6d129ca06db48ff8784445c5a3e7
liuyang <yang.liu01@enmotech.com>
2019-09-11 17:43:32 +0800

    重构测试用例对结果的string进行断言

--- class7_1/dictionary_test.go
+++ class7_1/dictionary_test.go
@@ -3,9 +3,14 @@ package class7_1
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionory := map[string]string{"test": "this is just a test"}
-	got := Search(dictionory, "test")
+	dictionary := map[string]string{"test": "this is just a test"}
+	got := Search(dictionary, "test")
 	want := "this is just a test"
+	assertString(got, want, t)
+}
+
+func assertString(got string, want string, t *testing.T) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %s want %s ", got, want)
 	}
