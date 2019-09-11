2a87135352dc48f6588331dec8b01abced69bf1f
huang peng <peng.huang2@enmotech.com>
2019-09-11 17:47:18 +0800

    map second

--- mapp/dictionary_test.go
+++ mapp/dictionary_test.go
@@ -6,6 +6,11 @@ func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just test"}
 	got := Search(dictionary, "test")
 	want := "this is just test"
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %s, want %s", got, want)
 	}
