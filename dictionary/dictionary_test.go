6cced7b03a3fe2163cdbf4c971fda398bb9a90a5
hongwei.wu <hongwei.wu@enmotech.com>
2019-09-11 17:43:14 +0800

    refine add assist func AssertStrings

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -8,6 +8,11 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
+	AssertStrings(t, got, want)
+}
+
+func AssertStrings(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got '%s' want '%s', given '%s'", got, want, "test")
 	}
