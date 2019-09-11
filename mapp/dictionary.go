f75d6a7a91bfe9851404bb5d8368d734b030a359
huang peng <peng.huang2@enmotech.com>
2019-09-11 18:01:45 +0800

    dictionary if key not exist response a error

--- mapp/dictionary.go
+++ mapp/dictionary.go
@@ -1,7 +1,13 @@
 package mapp
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (dictionary Dictionary) Search(key string) string {
-	return dictionary[key]
+func (dictionary Dictionary) Search(key string) (string, error) {
+	value, ok := dictionary[key]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return value, nil
 }
--- mapp/dictionary_test.go
+++ mapp/dictionary_test.go
@@ -4,9 +4,20 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just test"}
-	got := dictionary.Search("test")
-	want := "this is just test"
-	assertStrings(t, got, want)
+	t.Run("know word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just test"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknow word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+		if err == nil {
+			t.Fatalf("expected to get a error")
+		}
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
