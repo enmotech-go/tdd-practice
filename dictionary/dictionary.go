d383c34f7621eea581f91837a80f8bfe43c3bb45
yq <qin.yin@enmotech.com>
2019-09-11 17:56:14 +0800

    使用assertError捕捉错误

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -2,13 +2,15 @@ package dictionary
 
 import "errors"
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 	return definition, nil
 }
