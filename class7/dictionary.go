ca60a07f44ce41cf0f447280da3528ec8b4896a0
liuyang <yang.liu01@enmotech.com>
2019-09-11 08:54:46 +0800

    重构加入全局定义错误以便复用

--- class7/dictionary.go
+++ class7/dictionary.go
@@ -1,9 +1,18 @@
 package class7
 
+import "errors"
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 //Dictionary 自定义的map类型
 type Dictionary map[string]string
 
 //Search 自定义类型的方法
-func (d Dictionary) Search(word string) (value string) {
-	return d[word]
+func (d Dictionary) Search(word string) (value string, err error) {
+	value, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return
 }
--- class7/dictionary_test.go
+++ class7/dictionary_test.go
@@ -4,9 +4,21 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertStrings(t, err.Error(), want)
+
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
