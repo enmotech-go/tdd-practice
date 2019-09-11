ed8f970f76d1ef12c128a8e793bab98bdd5405f4
yq <qin.yin@enmotech.com>
2019-09-11 18:11:29 +0800

    使用assertDefinition断言

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -21,18 +21,11 @@ func TestSearch(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
-
-	if err != nil {
-		t.Fatal("should find added word:", err)
-	}
-
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+	assertDefinition(t, dictionary, word, definition)
 }
 
 func assertError(t *testing.T, got, want error) {
@@ -49,5 +42,18 @@ func assertStrings(t *testing.T, got, want string) {
 
 	if got != want {
 		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != definition {
+		t.Errorf("got '%s' want '%s'", got, definition)
 	}
 }
