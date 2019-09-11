28f1cfc6d239b24d82b917de79be2cb2644b2924
hongwei.wu <hongwei.wu@enmotech.com>
2019-09-11 17:54:27 +0800

    refine test case add AssertError

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -2,12 +2,14 @@ package dictionary
 
 import "errors"
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(key string) (string, error) {
 	definition, ok := d[key]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 	return definition, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -14,13 +14,7 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknown word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
-
-		if err == nil {
-			t.Fatal("expected to get an error.")
-		}
-
-		AssertStrings(t, err.Error(), want)
+		AssertError(t, err, ErrNotFound)
 	})
 }
 
@@ -28,5 +22,12 @@ func AssertStrings(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got '%s' want '%s', given '%s'", got, want, "test")
+	}
+}
+
+func AssertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
 	}
 }
