89f90c93cb0a694831223c7de487bc6049cd96cc
樊建 <jian.fan@enmotech.com>
2019-09-11 19:10:55 +0800

    new add function unit test case

--- class07/dictionary.go
+++ class07/dictionary.go
@@ -3,6 +3,7 @@ package class07
 import "errors"
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("cannot add word because it already exists")
 
 type Dictionary map[string]string
 
@@ -14,6 +15,15 @@ func (dic Dictionary) Search(keyword string) (result string, err error) {
 	return
 }
 
-func (dic Dictionary) Add(word, definition string) {
-	dic[word] = definition
+func (dic Dictionary) Add(word, definition string) (err error) {
+	_, err = dic.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		dic[word] = definition
+		err = nil
+	case nil:
+		return ErrWordExists
+	}
+	return
 }
--- class07/dictionary_test.go
+++ class07/dictionary_test.go
@@ -19,12 +19,26 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
-	assertDefinition(t, dictionary, word, definition)
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, definition)
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
 }
 
 func assertStrings(t *testing.T, got, want string) {
