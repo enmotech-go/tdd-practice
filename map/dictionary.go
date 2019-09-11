ac82186d65634e57f98bf17f6b3d3efd60b9a71e
daiwei <wei.dai@enmotech.com>
2019-09-11 17:52:22 +0800

    重构并单元测试通过

--- map/dictionary.go
+++ map/dictionary.go
@@ -1,5 +1,14 @@
 package _map
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -3,12 +3,24 @@ package _map
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+		assertStrings(t, got, want)
+	})
 
-	assertStrings(t, got, want)
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
