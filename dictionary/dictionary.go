3ea1d5d836f8ee42c0c243a800770c50c7cbebd1
hongwei.wu <hongwei.wu@enmotech.com>
2019-09-11 17:51:23 +0800

    Search method add error return value

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,7 +1,13 @@
 package dictionary
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(key string) string {
-	return d[key]
+func (d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	AssertStrings(t, got, want)
+		AssertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		AssertStrings(t, err.Error(), want)
+	})
 }
 
 func AssertStrings(t *testing.T, got, want string) {
