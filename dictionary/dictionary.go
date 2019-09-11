f0353b8e8bf6b930408c6dfe6efed3fb3d63bc71
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-09-11 17:55:52 +0800

    重构 测试通过

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,7 +1,14 @@
 package dictionary
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
