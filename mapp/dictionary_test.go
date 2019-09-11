314fb0cde5e4b6034919d632341bc074846d5af7
huang peng <peng.huang2@enmotech.com>
2019-09-11 18:40:21 +0800

    测试键添加、查询分开

--- mapp/dictionary_test.go
+++ mapp/dictionary_test.go
@@ -4,32 +4,19 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just test")
-	t.Run("know word", func(t *testing.T) {
-		got, _ := dictionary.Search("test")
-		want := "this is just test"
-		assertStrings(t, got, want)
-	})
-
-	t.Run("unknow word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		if err == nil {
-			t.Fatalf("expected to get a error")
-		}
-		assertError(t, err, NotExistError)
-	})
+	word := "test"
+	definition := "this is just test"
+	dictionary.Add(word, definition)
+	assertDefinition(t, dictionary, word, definition)
 }
 
-func assertStrings(t *testing.T, got, want string) {
+func assertDefinition(t *testing.T, d Dictionary, key, want string) {
 	t.Helper()
+	got, err := d.Search(key)
+	if err != nil {
+		t.Fatal("show find add word", err)
+	}
 	if got != want {
 		t.Errorf("got %s, want %s", got, want)
-	}
-}
-
-func assertError(t *testing.T, got, want error) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %s, but want %s", got, want)
 	}
 }
