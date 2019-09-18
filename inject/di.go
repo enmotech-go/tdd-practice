5e3e670865b6933a6d695c1474414e85a3ab25d1
huang peng <peng.huang2@enmotech.com>
2019-09-18 18:00:31 +0800

    di http server

--- inject/di.go
+++ inject/di.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "hello,%s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "errio")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
