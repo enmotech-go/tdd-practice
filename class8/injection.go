26ef682ca4f236d3130487389d64254b5336f1aa
liuyang <yang.liu01@enmotech.com>
2019-09-18 18:01:23 +0800

    尝试http

--- class8/injection.go
+++ class8/injection.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
 func Greet(buffer io.Writer, name string) {
 	fmt.Fprintf(buffer, "Hello ,%s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "Elodie")
+	http.ListenAndServe("127.0.0.1:5000", http.HandlerFunc(MyGreeterHandler))
 }
