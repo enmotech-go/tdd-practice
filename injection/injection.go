01c182153c93eeef877f621e71f8fd6c74158d9b
chenming <ming.chen@enmotech.com>
2019-09-18 17:52:56 +0800

    add main func

--- injection/injection.go
+++ injection/injection.go
@@ -1,10 +1,15 @@
-package injection
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer,"Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
