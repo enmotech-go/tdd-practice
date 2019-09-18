743f88fd2ba9fe3a35e8b8050de51b6fb6d7e43b
kliyes <tom@kliyes.com>
2019-09-18 17:48:26 +0800

    use io.Writer

--- di/di.go
+++ di/di.go
@@ -1,10 +1,15 @@
-package di
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Tom")
 }
