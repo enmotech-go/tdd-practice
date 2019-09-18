1f7fb79f929af29e6f097d22c3fdab3c040570d4
hongwei.wu <hongwei.wu@enmotech.com>
2019-09-18 17:43:49 +0800

    refine

--- di/di.go
+++ di/di.go
@@ -1,10 +1,10 @@
 package di
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
