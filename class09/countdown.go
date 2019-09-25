7f24ea96f8d5ef963feb707f6cd41128c98182f7
樊建 <jian.fan@enmotech.com>
2019-09-25 18:16:21 +0800

    consturct countdown function

--- class09/countdown.go
+++ class09/countdown.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+const finalWord = "Go!"
+const countdownStart = 3
 
 func main() {
 	Countdown(os.Stdout)
 }
 
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
 }
