fb6dd9752771ceece29490b1ebcabd7a29325bd7
kliyes <tom@kliyes.com>
2019-09-25 18:07:25 +0800

    refactor

--- mock/mock.go
+++ mock/mock.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
+)
+
+const (
+	countdownStart = 3
+	finalWord      = "Go!"
 )
 
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
