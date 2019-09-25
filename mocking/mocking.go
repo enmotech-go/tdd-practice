f647fff3c6a639e5a18bc403e495839f309ef03a
chenming <ming.chen@enmotech.com>
2019-09-25 18:05:27 +0800

    重构常量

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -6,11 +6,14 @@ import (
 	"os"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
