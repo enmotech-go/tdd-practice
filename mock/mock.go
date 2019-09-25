de487c19626c0fd5f4265e285ec32d2ea84a9c8f
daiwei <wei.dai@enmotech.com>
2019-09-25 18:15:07 +0800

    mock项目 第二步，打印3       2       1       Go!` 并单元测试通过

--- mock/mock.go
+++ mock/mock.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
