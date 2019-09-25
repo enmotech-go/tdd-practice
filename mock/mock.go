dc4f3d24c1e828fb554c2ce711466377f02e4db7
hongwei.wu <hongwei.wu@enmotech.com>
2019-09-25 17:53:11 +0800

    add sleeps

--- mock/mock.go
+++ mock/mock.go
@@ -1,10 +1,26 @@
-package mock
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
+	"time"
 )
 
-func Countdown(out *bytes.Buffer) {
-	fmt.Fprint(out, "3")
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(time.Second)
+		fmt.Fprintln(out, i)
+	}
+	time.Sleep(time.Second)
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
 }
