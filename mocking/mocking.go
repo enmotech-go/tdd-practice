57b9526842905ab17a7aae6d87ea47ddab632404
chenming <ming.chen@enmotech.com>
2019-09-25 18:06:10 +0800

    sleep加入

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -11,8 +12,10 @@ const countdownStart = 3
 
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(out, finalWord)
 }
 
