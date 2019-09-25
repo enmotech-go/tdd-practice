e6b1c14d8d6329a723e4b586dfd062df91858f5e
daiwei <wei.dai@enmotech.com>
2019-09-25 18:00:25 +0800

    添加mock项目 第一步，打印3，并单元测试通过

---
+++ mock/mock.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Countdown(out io.Writer) {
+	fmt.Fprint(out, "3")
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
---
+++ mock/mock_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	Countdown(buffer)
+	got := buffer.String()
+	want := "3"
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
