99036051a765acf54dbb93f47c13e83239d040a4
kliyes <tom@kliyes.com>
2019-09-25 18:01:47 +0800

    change buffer to writer

--- mock/mock.go
+++ mock/mock.go
@@ -1,10 +1,15 @@
-package mock
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Countdown(out *bytes.Buffer) {
+func Countdown(out io.Writer) {
 	fmt.Fprint(out, "3")
+}
+
+func main() {
+	Countdown(os.Stdout)
 }
