c64c337a1438e9aadf706c93d4385a0494967854
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-10-16 18:40:47 +0800

    aaa

--- concurrency/checkWebsites.go
+++ concurrency/checkWebsites.go
@@ -1,19 +1,24 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type Result struct {
+	Url       string
+	Reachable bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-
+	resultsChan := make(chan Result)
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultsChan <- Result{Url: u, Reachable: wc(u)}
 		}(url)
 	}
 
-	time.Sleep(3 * time.Second)
-
+	for i := len(urls); i > 0; i-- {
+		result := <-resultsChan
+		results[result.Url] = result.Reachable
+	}
 	return results
 }
