31c4d3ca2a04b3861d80bae9464e4be067a6910b
kliyes <tom@kliyes.com>
2019-10-16 18:20:45 +0800

    benchmark

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -33,5 +34,21 @@ func TestCheckWebsites(t *testing.T) {
 	}
 	if !reflect.DeepEqual(gotResults, wantResults) {
 		t.Fatalf("want %v, got %v", wantResults, gotResults)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
