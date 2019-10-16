3691eaf09ea6a4b821cf23ec3b91919367040015
yq <qin.yin@enmotech.com>
2019-10-16 18:41:14 +0800

    使用channel

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,18 +1,26 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		go func(url string) {
-			results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(5 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
+
 	return results
 }
