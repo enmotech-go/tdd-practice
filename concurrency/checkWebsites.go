1acd1f3bdfacc8f3ac79ef7da0e82e9f2bd500c7
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-10-16 18:26:42 +0800

    并行实现

--- concurrency/checkWebsites.go
+++ concurrency/checkWebsites.go
@@ -1,13 +1,19 @@
 package concurrency
 
+import "time"
+
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			results[u] = wc(u)
+		}(url)
 	}
+
+	time.Sleep(3 * time.Second)
 
 	return results
 }
--- concurrency/checkWebsites_test.go
+++ concurrency/checkWebsites_test.go
@@ -3,7 +3,13 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
 
 func mockWebsiteChecker(url string) bool {
 	if url == "waat://furhurterwe.geds" {
@@ -11,6 +17,18 @@ func mockWebsiteChecker(url string) bool {
 	}
 	return true
 }
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
+
 func TestCheckWebsites(t *testing.T) {
 	websites := []string{
 		"http://google.com",
@@ -23,7 +41,7 @@ func TestCheckWebsites(t *testing.T) {
 	want := len(websites)
 	got := len(actualResults)
 	if want != got {
-		t.Fatalf("Wanted %v, got %v", want, got)
+		t.Fatalf("Wanted %v, got %v", websites, actualResults)
 	}
 
 	expectedResults := map[string]bool{
