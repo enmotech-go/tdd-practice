60f782d821941bbcff73f9c55e4842d7fa7fdb1d
yq <qin.yin@enmotech.com>
2019-10-16 18:27:24 +0800

    使用go并发处理

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,13 +1,18 @@
 package concurrency
 
+import "time"
+
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(url string) {
+			results[url] = wc(url)
+		}(url)
 	}
 
+	time.Sleep(5 * time.Second)
 	return results
 }
