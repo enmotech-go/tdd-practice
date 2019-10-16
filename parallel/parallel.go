e5258aa2d48591ee73b958b92706679337c2e716
huang peng <peng.huang2@enmotech.com>
2019-10-16 18:44:20 +0800

    second

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -2,11 +2,23 @@ package parallel
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-
+	rst := make(chan result)
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(url string) {
+			rst <- result{url, wc(url)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-rst
+		results[r.string] = r.bool
 	}
 
 	return results
--- parallel/parallel_test.go
+++ parallel/parallel_test.go
@@ -1,6 +1,7 @@
 package parallel
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -18,5 +19,38 @@ func BenchmarkCheckWebsites(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
+
+func mockWebsiteChecker(url string) bool {
+    if url == "waat://furhurterwe.geds" {
+        return false
+    }
+    return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	actualResults := CheckWebsites(mockWebsiteChecker, websites)
+
+	want := len(websites)
+	got := len(actualResults)
+	if want != got {
+		t.Fatalf("Wanted %v, got %v", want, got)
+	}
+
+	expectedResults := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	if !reflect.DeepEqual(expectedResults, actualResults) {
+		t.Fatalf("Wanted %v, got %v", expectedResults, actualResults)
 	}
 }
