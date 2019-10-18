5202ee2dc45a8374191f721f40dfe11f58293099
kliyes <tom@kliyes.com>
2019-10-18 21:26:02 +0800

    fizzbuzz refactor

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -11,16 +11,20 @@ const (
 	WordFizzBuzz = WordFizz + WordBuzz
 )
 
+func Converter(i int) string {
+	if i%3 == 0 && i%5 == 0 {
+		return WordFizzBuzz
+	} else if i%3 == 0 {
+		return WordFizz
+	} else if i%5 == 0 {
+		return WordBuzz
+	} else {
+		return fmt.Sprintf("%d", i)
+	}
+}
+
 func FizzBuzz(w io.Writer, n int) {
 	for i := 1; i < n+1; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Fprintf(w, "%s\n", WordFizzBuzz)
-		} else if i%3 == 0 {
-			fmt.Fprintf(w, "%s\n", WordFizz)
-		} else if i%5 == 0 {
-			fmt.Fprintf(w, "%s\n", WordBuzz)
-		} else {
-			fmt.Fprintf(w, "%d\n", i)
-		}
+		fmt.Fprintf(w, "%s\n", Converter(i))
 	}
 }
