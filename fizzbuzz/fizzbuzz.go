6e89f85ea007196631b27e79d65bdb867efc6121
hongwei.wu <hongwei.wu@enmotech.com>
2019-10-23 18:04:46 +0800

    add more test, number divisible by 3、5、15

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,5 +1,14 @@
 package fizzbuzz
 
-func FizzBuzz(s string) string {
-	return s
+import "fmt"
+
+func FizzBuzz(s int64) string {
+	if s%15 == 0 {
+		return "fizzbuzz"
+	} else if s%3 == 0 {
+		return "fizz"
+	} else if s%5 == 0 {
+		return "buzz"
+	}
+	return fmt.Sprintf("%d", s)
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -2,12 +2,27 @@ package fizzbuzz
 
 import "testing"
 
+func assertEqual(t *testing.T, result, expect string) {
+	t.Helper()
+	if result != expect {
+		t.Errorf("result(%s) != expect(%s)", result, expect)
+	}
+}
+
 func TestFizzBuzz(t *testing.T) {
 	t.Run("test should return raw number", func(t *testing.T) {
-		result := FizzBuzz("1")
-		expect := "1"
-		if result != expect {
-			t.Errorf("result(%s) != expect(%s)", result, expect)
-		}
+		assertEqual(t, FizzBuzz(1), "1")
+	})
+
+	t.Run("test show return fizz", func(t *testing.T) {
+		assertEqual(t, FizzBuzz(3), "fizz")
+	})
+
+	t.Run("test show return buzz", func(t *testing.T) {
+		assertEqual(t, FizzBuzz(5), "buzz")
+	})
+
+	t.Run("test show return fizzbuzz", func(t *testing.T) {
+		assertEqual(t, FizzBuzz(15), "fizzbuzz")
 	})
 }
