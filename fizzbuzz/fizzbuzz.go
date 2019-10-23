358ea75b1976c103365fe1aa37a7b5454c78060e
kliyes <tom@kliyes.com>
2019-10-23 17:49:42 +0800

    add fizzbuzz

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -2,23 +2,10 @@ package fizzbuzz
 
 import "fmt"
 
-const (
-	WordFizz = "Fizz"
-	WordBuzz = "Buzz"
-)
+type FizzBuzz struct {
+	input int
+}
 
-type FizzBuzz struct{}
-
-func (FizzBuzz) Convert(n int) string {
-	var result string
-	if n%3 == 0 {
-		result += WordFizz
-	}
-	if n%5 == 0 {
-		result += WordBuzz
-	}
-	if result == "" {
-		result = fmt.Sprintf("%d", n)
-	}
-	return result
+func (fb FizzBuzz) Number() string {
+	return fmt.Sprintf("%d", fb.input)
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -1,47 +1,17 @@
 package fizzbuzz
 
-import "testing"
+import (
+	"testing"
 
-func TestFizzBuzz_Convert(t *testing.T) {
-	type args struct {
-		n int
-	}
-	tests := []struct {
-		name string
-		f    FizzBuzz
-		args args
-		want string
-	}{
-		{
-			"test_3",
-			FizzBuzz{},
-			args{n: 3},
-			WordFizz,
-		},
-		{
-			"test_5",
-			FizzBuzz{},
-			args{n: 5},
-			WordBuzz,
-		},
-		{
-			"test_15",
-			FizzBuzz{},
-			args{n: 15},
-			WordFizz + WordBuzz,
-		},
-		{
-			"test_others",
-			FizzBuzz{},
-			args{n: 4},
-			"4",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.f.Convert(tt.args.n); got != tt.want {
-				t.Errorf("FizzBuzz.Convert() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFizzBuzz(t *testing.T) {
+	t.Run("test_should_show_raw_number", func(t *testing.T) {
+		fb := FizzBuzz{1}
+		assert.Equal(t, "1", fb.Number())
+
+		fb = FizzBuzz{2}
+		assert.Equal(t, "2", fb.Number())
+	})
 }
