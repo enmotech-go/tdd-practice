1ab39d7562fa55a0f9487327041ac252d2710804
liuyang <yang.liu01@enmotech.com>
2019-10-25 11:00:45 +0800

    add httptest

---
+++ class12/select_test.go
@@ -0,0 +1,29 @@
+package class12
+
+import (
+	"github.com/magiconair/properties/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+	want := fastURL
+	got := Racer(fastURL, slowURL)
+	assert.Equal(t, got, want)
+	slowServer.Close()
+	fastServer.Close()
+}
