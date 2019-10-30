56308558e028b40cee6438e66848f6a93b75d5c4
kliyes <tom@kliyes.com>
2019-10-30 18:27:35 +0800

    add select case

--- select_racer/select.go
+++ select_racer/select.go
@@ -2,21 +2,22 @@ package select_racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) string {
-	aDuration := measureResposeTime(a)
-	bDuration := measureResposeTime(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureResposeTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
