b7d4ca533710544137ae2f79bc5207b6a7b620e5
kliyes <tom@kliyes.com>
2019-10-30 18:10:53 +0800

    add select racer

---
+++ select_racer/select_test.go
@@ -0,0 +1,26 @@
+package select_racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	assert.Equal(t, fastServer.URL, Racer(slowServer.URL, fastServer.URL))
+
+	slowServer.Close()
+	fastServer.Close()
+}
