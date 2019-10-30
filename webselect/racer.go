30df51395f76c45db5a2792fad5afd2b5130ee90
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-10-30 18:26:57 +0800

    select

--- webselect/racer.go
+++ webselect/racer.go
@@ -2,22 +2,22 @@ package webselect
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
