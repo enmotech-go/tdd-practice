cd44cf0a3ca126e04029fe64b2c9a5d070fe5093
chenming <ming.chen@enmotech.com>
2019-10-30 18:07:20 +0800

    用httptest 来模拟测试

--- select_tdd/select_test.go
+++ select_tdd/select_test.go
@@ -1,10 +1,25 @@
 package select_tdd
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "https://github.com/"
-	fastURL := "http://www.163.com"
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,6 +27,9 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
 
 
