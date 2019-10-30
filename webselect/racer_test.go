3527a7f2030b819381593aa43abd3044a9a15f31
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-10-30 18:12:11 +0800

    首次提交

---
+++ webselect/racer_test.go
@@ -0,0 +1,15 @@
+package webselect
+
+import "testing"
+
+func TestRacer(t *testing.T) {
+	slowURL := "http://www.facebook.com"
+	fastURL := "http://www.quii.co.uk"
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
