3366911034f051e0eabce2f64ba1dc31e5418ef9
huang peng <peng.huang2@enmotech.com>
2019-10-30 18:23:51 +0800

    no repeat code

--- selectpractise/selectp.go
+++ selectpractise/selectp.go
@@ -6,14 +6,16 @@ import (
 	)
 
 func Racer(a, b string) string {
-	startA:= time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 	if aDuration < bDuration {
 		return a
 	}
 	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	startA:= time.Now()
+	http.Get(url)
+	return time.Since(startA)
 }
--- selectpractise/selectp_test.go
+++ selectpractise/selectp_test.go
@@ -7,16 +7,18 @@ import (
 	"testing"
 )
 
-func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		time.Sleep(20 * time.Millisecond)
+func makeDelayedServer(delay time.Duration) *httptest.Server  {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
 	}))
+}
 
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+	defer slowServer.Close()
+	defer fastServer.Close()
 	slowUrl := slowServer.URL
 	fastUrl := fastServer.URL
 	want := fastUrl
