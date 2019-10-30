2fcd04215eb782946c165761e9bda962020a0609
yq <qin.yin@enmotech.com>
2019-10-30 18:18:19 +0800

    使用http重构代码

---
+++ websiteracer/websiteracer.go
@@ -0,0 +1,22 @@
+package websiteracer
+
+import (
+	"net/http"
+	"time"
+)
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
+
+func Racer(a, b string) string {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+}
---
+++ websiteracer/websiteracer_test.go
@@ -0,0 +1,34 @@
+package websiteracer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
+
+func TestRacer(t *testing.T) {
+
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	A := slowServer.URL
+	B := fastServer.URL
+
+	want := B
+	got := Racer(A, B)
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
