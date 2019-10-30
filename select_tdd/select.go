ed1046577c9366cce4cdb9019b303737c52202f2
chenming <ming.chen@enmotech.com>
2019-10-30 18:29:58 +0800

    重构加入timeout，加入测试用例

--- select_tdd/select.go
+++ select_tdd/select.go
@@ -1,14 +1,25 @@
 package select_tdd
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
-func Racer(a, b string) (winner string) {
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
--- select_tdd/select_test.go
+++ select_tdd/select_test.go
@@ -8,25 +8,39 @@ import (
 )
 
 func TestRacer(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
 
-	if got != want {
-		t.Errorf("got '%s', want '%s'", got, want)
-	}
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	})
 
-	slowServer.Close()
-	fastServer.Close()
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 
