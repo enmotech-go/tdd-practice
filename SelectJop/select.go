84ee4f1ceece4402a68f5bc32efb65245f9f9210
bk_gitlab <kai.bai@enmotech.com>
2019-10-30 18:28:24 +0800

    select使用

--- SelectJop/select.go
+++ SelectJop/select.go
@@ -2,22 +2,27 @@ package SelectJop
 
 import (
 	"net/http"
-	"time"
 )
 
-func Racer(a,b string) (winner string)  {
-
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration<bDuration{
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
-
 }
-func measureResponseTime(url string) time.Duration  {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+
+//func measureResponseTime(url string) time.Duration {
+//	start := time.Now()
+//	http.Get(url)
+//	return time.Since(start)
+//}
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
