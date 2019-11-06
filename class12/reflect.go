e9de9f17b14a3355b4026700d5009046a4e0da71
樊建 <jian.fan@enmotech.com>
2019-11-06 18:37:17 +0800

    construct walk function

--- class12/reflect.go
+++ class12/reflect.go
@@ -5,26 +5,25 @@ import "reflect"
 func Walk(x interface{}, fn func(string)) {
 	val := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
+	walkValue := func(value reflect.Value) {
+		Walk(value.Interface(), fn)
+	}
 
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
 	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
 	case reflect.Map:
 		for _, key := range val.MapKeys() {
-			Walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
 		}
-	}
-
-	for i := 0; i < numberOfValues; i++ {
-		Walk(getField(i).Interface(), fn)
 	}
 }
 
