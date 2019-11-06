fbdf5077594f4ab89a206a15e45f81471ee1040f
hongwei.wu <hongwei.wu@enmotech.com>
2019-11-06 17:56:51 +0800

    refine func walk, use switch replace if else if

--- reflect/reflect.go
+++ reflect/reflect.go
@@ -6,9 +6,10 @@ func walk(x interface{}, fn func(string)) {
 	val := reflect.ValueOf(x)
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		if field.Kind() == reflect.String {
+		switch field.Kind() {
+		case reflect.String:
 			fn(field.String())
-		} else if field.Kind() == reflect.Struct {
+		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 	}
