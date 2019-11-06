c7c44ad3104f659611ee6dfab0925f3942a5400c
liuyang <yang.liu01@enmotech.com>
2019-11-06 18:58:51 +0800

    recreate test  walk add maps

--- class13_1/reflecttion.go
+++ class13_1/reflecttion.go
@@ -4,6 +4,9 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 	numberOfValues := 0
 	var getField func(int2 int) reflect.Value
 
@@ -11,14 +14,16 @@ func walk(x interface{}, fn func(input string)) {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
 	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
 	case reflect.Map:
 		for _, key := range val.MapKeys() {
-			walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
 		}
 
 	}
--- class13_1/reflecttion_test.go
+++ class13_1/reflecttion_test.go
@@ -65,4 +65,31 @@ func TestWalk(t *testing.T) {
 		})
 	}
 
+	t.Run("with Maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+
+		})
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain '%s' but it didnt", haystack, needle)
+	}
 }
