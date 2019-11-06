c73117017a362d2a99cd66807f4c32a92424bcd2
hongwei.wu <hongwei.wu@enmotech.com>
2019-11-06 17:50:10 +0800

    add more test case, refine func walk

--- reflect/reflect_test.go
+++ reflect/reflect_test.go
@@ -18,6 +18,14 @@ func TestWalk(t *testing.T) {
 			}{"Chris"},
 			[]string{"Chris"},
 		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
 	}
 
 	for _, test := range cases {
