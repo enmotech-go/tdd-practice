38658dd5fb7a96b41d3cdb53a4bc5d8704a6dbf9
liuyang <yang.liu01@enmotech.com>
2019-11-06 18:19:10 +0800

    recreate func  walk add func getValue

--- class13_1/reflecttion.go
+++ class13_1/reflecttion.go
@@ -3,23 +3,26 @@ package class13_1
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-
-		if field.Kind() == reflect.String {
+		switch field.Kind() {
+		case reflect.String:
 			fn(field.String())
-
-		}
-
-		if field.Kind() == reflect.Struct {
+		case reflect.Struct:
 			walk(field.Interface(), fn)
+
 		}
+
 	}
 
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
 }
