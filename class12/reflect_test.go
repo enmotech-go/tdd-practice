c5525b5cc0a95da196af7a22c52c7c3769712521
樊建 <jian.fan@enmotech.com>
2019-11-06 18:03:25 +0800

    construct walk function unit test

--- class12/reflect_test.go
+++ class12/reflect_test.go
@@ -1,26 +1,34 @@
 package class12
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
-
-	x := struct {
-		Name string
-	}{expected}
-
-	Walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("got '%d' want '%d'", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct witch one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
 	}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			Walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
 
-	if got[0] != expected {
-		t.Errorf("got '%s' want '%s'", got[0], expected)
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got '%v' want '%v'", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
