7c49348f975c517450920dbbecd0af9a8767e018
daiwei <wei.dai@enmotech.com>
2019-11-06 17:56:20 +0800

    重构项目 reflection 添加反射 并单元测试通过

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,5 +1,9 @@
 package reflection
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+	val := reflect.ValueOf(x)
+	filed := val.Field(0)
+	fn(filed.String())
 }
