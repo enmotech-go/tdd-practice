16c4cfc5d4692d10a2e0b37a39c52098d4a1aefe
huang peng <peng.huang2@enmotech.com>
2019-11-06 18:01:10 +0800

    reflect 2

--- reflectp/reflectp_test.go
+++ reflectp/reflectp_test.go
@@ -1,22 +1,35 @@
 package reflectp
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
-	x := struct {
+	cases := []struct{
 		Name string
-	}{expected}
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+		Input interface{}
+		ExpectedCalls []string
+	} {
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
 	}
-
-	if got[0] != expected {
-		t.Errorf("got '%s', want '%s'", got[0], expected)
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
