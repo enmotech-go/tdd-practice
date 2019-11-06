ea4aae24319adfbc056ff4ef6f00507c09728be8
hongwei.wu <hongwei.wu@enmotech.com>
2019-11-06 18:06:09 +0800

    refine func walk

--- reflect/reflect.go
+++ reflect/reflect.go
@@ -4,21 +4,17 @@ import "reflect"
 
 func walk(x interface{}, fn func(string)) {
 	val := getValue(x)
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
 		}
-		return
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
 		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
