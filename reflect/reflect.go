3cd3988fca8f6135aa5e9dee3ac44e8e51dfe7ce
hongwei.wu <hongwei.wu@enmotech.com>
2019-11-06 17:52:07 +0800

    refine func walk, judge field type

--- reflect/reflect.go
+++ reflect/reflect.go
@@ -6,6 +6,8 @@ func walk(x interface{}, fn func(string)) {
 	val := reflect.ValueOf(x)
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		fn(field.String())
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
 	}
 }
