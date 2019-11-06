7eb3f32b43bacdfa24525aaf75173fb5bcbb8b83
chenming <ming.chen@enmotech.com>
2019-11-06 17:54:01 +0800

    reflect 加入多字符串测试用例

--- reflect/reflect.go
+++ reflect/reflect.go
@@ -5,7 +5,9 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
 
+	for i:=0; i<val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
 }
