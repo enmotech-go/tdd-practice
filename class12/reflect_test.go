33ad2af53d07e4686fbfb81ae424891aaae01471
樊建 <jian.fan@enmotech.com>
2019-11-06 18:38:21 +0800

    add walk map parameter unit test case

--- class12/reflect_test.go
+++ class12/reflect_test.go
@@ -96,4 +96,30 @@ func TestWalk(t *testing.T) {
 			}
 		})
 	}
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		Walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain '%s' but it didnt", haystack, needle)
+	}
 }
