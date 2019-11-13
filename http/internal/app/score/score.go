4315bb2f8c4913c5534af06048c0c6ee74584760
hongwei.wu <hongwei.wu@enmotech.com>
2019-11-13 18:15:11 +0800

    refine PlayerServer

--- http/internal/app/score/score.go
+++ http/internal/app/score/score.go
@@ -14,11 +14,21 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
 	}
+}
 
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
