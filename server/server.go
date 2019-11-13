384cfdadd1b2c478b049466f9f8b2cf6aaa647a7
bk_gitlab <kai.bai@enmotech.com>
2019-11-13 18:02:16 +0800

    server

--- server/server.go
+++ server/server.go
@@ -1,1 +1,23 @@
-package server
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+
+	return ""
+}
--- server/server_test.go
+++ server/server_test.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test(t *testing.T)  {
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "20")
+	})
+
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+	})
+}
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got '%s' want '%s'", got, want)
+	}
+}
