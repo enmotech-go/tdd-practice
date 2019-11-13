4b9b3ba2fb28f4b0ab500060d989655c737c990e
bk_gitlab <kai.bai@enmotech.com>
2019-11-13 18:39:24 +0800

    server

--- server/server.go
+++ server/server.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
 
-	fmt.Fprint(w, GetPlayerScore(player))
+type PlayerStore interface {
+	GetPlayerScore(name string) int
 }
+
+type PlayerServer struct {
+	store PlayerStore
+}
+//func PlayerServer(w http.ResponseWriter, r *http.Request) {
+//	player := r.URL.Path[len("/players/"):]
+//
+//	fmt.Fprint(w, GetPlayerScore(player))
+//}
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
 
 func GetPlayerScore(name string) string {
 	if name == "Pepper" {
@@ -20,4 +34,16 @@ func GetPlayerScore(name string) string {
 	}
 
 	return ""
+}
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
--- server/server_test.go
+++ server/server_test.go
@@ -6,13 +6,28 @@ import (
 	"net/http/httptest"
 	"testing"
 )
+type StubPlayerStore struct {
+	scores map[string]int
+}
 
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
 func Test(t *testing.T)  {
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+	server := &PlayerServer{&store}
+
 	t.Run("returns Pepper's score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
 		assertResponseBody(t, response.Body.String(), "20")
 	})
@@ -21,7 +36,7 @@ func Test(t *testing.T)  {
 		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 
 		assertResponseBody(t, response.Body.String(), "10")
 	})
