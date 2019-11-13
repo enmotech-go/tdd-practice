b1b4ef53db7e2f5b5ea707866ef06a86be3e6f60
kliyes <tom@kliyes.com>
2019-11-13 17:49:17 +0800

    serve

--- server/server.go
+++ server/server.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+}
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "20")
