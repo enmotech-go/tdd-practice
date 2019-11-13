8843edd785f5d6b60c435eb4f5169797d8cc99c1
jiahuae <jiahua.zhong@enmotech.com>
2019-11-13 18:18:06 +0800

    player

--- class14/server.go
+++ class14/server.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+func main() {
+	playerServer := PlayerServer{}
+	playerServer.store = &StubPlayerStore{}
+	handler := http.HandlerFunc(playerServer.ServeHTTP)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
 }
