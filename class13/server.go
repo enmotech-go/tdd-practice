986a35dd36c8e5afee13f81f20e23a7cb491e6ac
yq <qin.yin@enmotech.com>
2019-11-13 19:10:34 +0800

    重构代码

--- class13/server.go
+++ class13/server.go
@@ -46,16 +46,17 @@ func (s *StubPlayerStore) GetPlayerScore(name string) int {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, player)
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 	}
 }
 
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -64,8 +65,7 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
