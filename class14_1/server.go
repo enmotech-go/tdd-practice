558014d3ff042b5bb5e153f9a247afccc850fe7f
liuyang <yang.liu01@enmotech.com>
2019-11-13 18:18:35 +0800

    add success test

--- class14_1/server.go
+++ class14_1/server.go
@@ -5,27 +5,43 @@ import (
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-
-	player := r.URL.Path[len("/player/"):]
-	getPlayerScore(player, w)
-
-}
-
-func getPlayerScore(player string, w http.ResponseWriter) {
+func GetPlayerScore(player string) string {
 	if player == "Pepper" {
-		fmt.Fprint(w, "20")
-		return
+
+		return "20"
 	}
 	if player == "Floyd" {
-		fmt.Fprint(w, "10")
-		return
+
+		return "10"
 	}
+	return ""
 }
 
 func main() {
-	handlerFunc := http.HandlerFunc(PlayerServer)
+	var server = &PlayerServer{}
+	handlerFunc := http.HandlerFunc(server.ServerHTTP)
 	if err := http.ListenAndServe(":5000", handlerFunc); err != nil {
 		return
 	}
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+type StubPlayerStore struct {
+	score map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	return s.score[name]
+}
+
+func (p *PlayerServer) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/player/"):]
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
--- class14_1/server_test.go
+++ class14_1/server_test.go
@@ -8,11 +8,16 @@ import (
 )
 
 func TestServer(t *testing.T) {
-
+	stub := StubPlayerStore{score: map[string]int{
+		"Pepper": 20,
+		"Floyd":  10,
+	}}
+	server := &PlayerServer{&stub}
 	t.Run("return Pepper Score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
 		recorder := httptest.NewRecorder()
-		PlayerServer(recorder, request)
+		server.ServerHTTP(recorder, request)
+		assertResponseBody(recorder.Body.String(), "20", t)
 		got := recorder.Body.String()
 		want := "20"
 		if got != want {
@@ -22,16 +27,20 @@ func TestServer(t *testing.T) {
 	t.Run("return Floyd Score", func(t *testing.T) {
 		request := newGetScoreRequest("Floyd")
 		recorder := httptest.NewRecorder()
-		PlayerServer(recorder, request)
-		got := recorder.Body.String()
-		want := "10"
-		if got != want {
-			t.Errorf("got  '%s'  want '%s' ", got, want)
-		}
+		server.ServerHTTP(recorder, request)
+		assertResponseBody(recorder.Body.String(), "10", t)
+
 	})
 }
 
 func newGetScoreRequest(name string) *http.Request {
 	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/player/%s", name), nil)
 	return request
+}
+
+func assertResponseBody(got, want string, t *testing.T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
 }
