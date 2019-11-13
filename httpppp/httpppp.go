57984de786345e2eaf1d1f859095a71d3f6e7a69
hongtao.zhao <hongtao.zhao@enmotech.com>
2019-11-13 17:58:34 +0800

    测试通过

---
+++ httpppp/httpppp.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
+}
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
