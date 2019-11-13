5161574c1a137ed12dff272fee91b6626167c64a
樊建 <jian.fan@enmotech.com>
2019-11-13 18:11:46 +0800

    construct PlayerServer

--- class13/server.go
+++ class13/server.go
@@ -5,6 +5,32 @@ import (
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+func main() {
+	handler := &PlayerServer{&StubPlayerStore{scores: map[string]int{}}}
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		return
+	}
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
