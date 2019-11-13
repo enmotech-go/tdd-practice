83f65cb54f871ca20a8cfae91c28ec4f6c69ac57
chenming <ming.chen@enmotech.com>
2019-11-13 18:56:17 +0800

    重构get和post路由

--- http/http.go
+++ http/http.go
@@ -22,13 +22,18 @@ type PlayerStore interface {
 type PlayerServer struct {
 	store PlayerStore
 }
-
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
 	}
 
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 
 	score := p.store.GetPlayerScore(player)
@@ -38,6 +43,10 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func GetPlayerScore(name string) string {
