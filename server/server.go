77ea47f1320868644b1161243a0a9d53e6c179b3
bk_gitlab <kai.bai@enmotech.com>
2019-11-20 18:48:20 +0800

    over

--- server/server.go
+++ server/server.go
@@ -97,7 +97,11 @@ type InMemoryPlayerStore struct{
 }
 
 func (i *InMemoryPlayerStore) GetLeague() []Player {
-	return nil
+	var league []Player
+	for name ,wins :=range i.store{
+		league=append(league,Player{name,wins})
+	}
+	return league
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
