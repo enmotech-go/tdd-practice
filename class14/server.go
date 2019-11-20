77d8cd5542da9403b253a9ba43c63913f126cce6
jiahuae <jiahua.zhong@enmotech.com>
2019-11-20 18:28:13 +0800

    helper func

--- class14/server.go
+++ class14/server.go
@@ -61,6 +61,7 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 
 //json
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
 	json.NewEncoder(w).Encode(p.store.GetLeague())
 	w.WriteHeader(http.StatusOK)
 }
