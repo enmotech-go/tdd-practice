fbcda956d9ae1faaae9cfd8a9508ff83557c3a5a
hongwei.wu <hongwei.wu@enmotech.com>
2019-11-20 17:53:48 +0800

    refine add getLeague

--- http/score.go
+++ http/score.go
@@ -26,12 +26,14 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
-	leagueTable := []Player{
+	json.NewEncoder(w).Encode(p.getLeagueTable())
+	w.WriteHeader(http.StatusOK)
+}
+
+func (p *PlayerServer) getLeagueTable() []Player {
+	return []Player{
 		{"Chris", 20},
 	}
-
-	json.NewEncoder(w).Encode(leagueTable)
-	w.WriteHeader(http.StatusOK)
 }
 
 func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
