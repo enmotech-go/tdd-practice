538643ac090befef97774d721f86ad20cde17f45
daiwei <wei.dai@enmotech.com>
2019-11-27 18:05:13 +0800

    项目 player 存储数据

---
+++ player/FileSystemStore.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+)
+
+type FileSystemStore struct {
+	database io.ReadSeeker
+}
+
+func (f *FileSystemStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
+	return league
+}
