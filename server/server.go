bec9faa0007bfe8ac875d5e0aa4bea8118a07482
kliyes <tom@kliyes.com>
2019-11-27 18:09:30 +0800

    add GetPlayerScore method

--- server/server.go
+++ server/server.go
@@ -121,3 +121,15 @@ func (f *FileSystemStore) GetLeague() []Player {
 	league, _ := NewLeague(f.database)
 	return league
 }
+
+func (f *FileSystemStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+	return wins
+}
