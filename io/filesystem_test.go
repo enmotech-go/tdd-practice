23eb9f14b471076a97ff876491f801b92a34fdaa
chenming <ming.chen@enmotech.com>
2019-11-27 17:56:24 +0800

    加入GetPlayerScore 函数

--- io/filesystem_test.go
+++ io/filesystem_test.go
@@ -26,4 +26,20 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+
+		store := FileSystemStore{database}
+
+		got := store.GetPlayerScore("Chris")
+
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
