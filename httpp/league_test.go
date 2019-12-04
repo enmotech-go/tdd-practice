468c92f9c332c6310fd7a15f14513f392aa0c97c
huang peng <peng.huang2@enmotech.com>
2019-12-04 18:15:49 +0800

    ok

--- httpp/league_test.go
+++ httpp/league_test.go
@@ -116,8 +116,8 @@ func TestFileSystemStore(t *testing.T) {
 		}
 		got := store.GetLeague()
 		want := League{
-			{"Cleo", 10},
 			{"Chris", 33},
+			{"Cleo", 10},
 		}
 		assert.Equal(t, want, got)
 		// read again
