ecf69b0c23a584ec90edee6992ddb23f6ac6d205
赵洪涛 <hongtao.zhao@enmotech.com>
2019-12-09 14:52:13 +0800

    代码提交

--- httpppp/league.go
+++ httpppp/league.go
@@ -6,9 +6,24 @@ import (
 	"io"
 )
 
-func NewLeague(rdr io.Reader) ([]Player, error) {
+// League stores a collection of players
+type League []Player
+
+// Find tries to return a player from a league
+func (l League) Find(name string) *Player {
+	for i, p := range l {
+		if p.Name == name {
+			return &l[i]
+		}
+	}
+	return nil
+}
+
+// NewLeague creates a league from JSON
+func NewLeague(rdr io.Reader) (League, error) {
 	var league []Player
 	err := json.NewDecoder(rdr).Decode(&league)
+
 	if err != nil {
 		err = fmt.Errorf("problem parsing league, %v", err)
 	}
