31a95f5ebc409b8d64e6b2ed55c1ab89d31ce904
hongwei.wu <hongwei.wu@enmotech.com>
2019-12-11 18:00:25 +0800

    refine

--- http/cli.go
+++ http/cli.go
@@ -1,9 +1,12 @@
 package http
 
+import "io"
+
 type CLI struct {
 	playerStore PlayerStore
+	in          io.Reader
 }
 
 func (cli *CLI) PlayPoker() {
-	cli.playerStore.RecordWin("Cleo")
+	cli.playerStore.RecordWin("Chris")
 }
--- http/cli_test.go
+++ http/cli_test.go
@@ -1,13 +1,28 @@
 package http
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("Chris wins\n")
 	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
+
+	cli := &CLI{playerStore, in}
 	cli.PlayPoker()
 
-	if len(playerStore.winCalls) != 1 {
-		t.Fatal("expected a win call but didn't get any")
+	assertPlayerWin(t, playerStore, "Chris")
+}
+
+func assertPlayerWin(t *testing.T, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
+		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("did not store correct winner got '%s' want '%s'", store.winCalls[0], winner)
 	}
 }
