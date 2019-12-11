6115fe9e8e562709bca33c46dbd3555c02ea0027
kliyes <tom@kliyes.com>
2019-12-11 18:01:02 +0800

    pass

--- server/cli_test.go
+++ server/cli_test.go
@@ -1,15 +1,21 @@
 package poker
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCli(t *testing.T) {
+	in := strings.NewReader("Chris wins\n")
 	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
+	cli := &CLI{playerStore, in}
 	cli.PlayPoker()
 
-	assert.Len(t, playerStore.winCalls, 1)
+	assert.GreaterOrEqual(t, len(playerStore.winCalls), 1)
+
+	got := playerStore.winCalls[0]
+	want := "Chris"
+	assert.Equal(t, want, got)
 }
