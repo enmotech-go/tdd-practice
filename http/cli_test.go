c7dddbd79e4f5b77e8d41630ebaa32ed55964143
hongwei.wu <hongwei.wu@enmotech.com>
2019-12-11 18:18:47 +0800

    change travis

--- http/cli_test.go
+++ http/cli_test.go
@@ -6,13 +6,26 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader("Chris wins\n")
-	playerStore := &StubPlayerStore{}
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
 
-	cli := &CLI{playerStore, in}
-	cli.PlayPoker()
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
 
-	assertPlayerWin(t, playerStore, "Chris")
+		assertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &StubPlayerStore{}
+
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerStore, "Cleo")
+	})
+
 }
 
 func assertPlayerWin(t *testing.T, store *StubPlayerStore, winner string) {
