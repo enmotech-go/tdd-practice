7cb18654a7b60f85657ccb0ae4795cdeeb59e4c6
kliyes <tom@kliyes.com>
2019-12-11 18:17:29 +0800

    refactor

--- server/cli.go
+++ server/cli.go
@@ -1,12 +1,33 @@
 package poker
 
-import "io"
+import (
+	"bufio"
+	"io"
+	"strings"
+)
 
 type CLI struct {
 	playerStore PlayerStore
-	in          io.Reader
+	in          *bufio.Scanner
+}
+
+func NewCLI(store PlayerStore, in io.Reader) *CLI {
+	return &CLI{
+		playerStore: store,
+		in:          bufio.NewScanner(in),
+	}
 }
 
 func (cli *CLI) PlayPoker() {
-	cli.playerStore.RecordWin("Chris")
+	userInput := cli.readLine()
+	cli.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func (cli *CLI) readLine() string {
+	cli.in.Scan()
+	return cli.in.Text()
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
 }
--- server/cli_test.go
+++ server/cli_test.go
@@ -7,15 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func assertPlayerWin(t *testing.T, store *StubPlayerStore, winner string) {
+	t.Helper()
+	assert.Len(t, store.winCalls, 1)
+	assert.Equal(t, winner, store.winCalls[0])
+}
+
 func TestCli(t *testing.T) {
-	in := strings.NewReader("Chris wins\n")
-	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore, in}
-	cli.PlayPoker()
+	t.Run("record_chris_win_from_user_input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
+		assertPlayerWin(t, playerStore, "Chris")
+	})
 
-	assert.GreaterOrEqual(t, len(playerStore.winCalls), 1)
-
-	got := playerStore.winCalls[0]
-	want := "Chris"
-	assert.Equal(t, want, got)
+	t.Run("record_cleo_win_from_user_input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
+		assertPlayerWin(t, playerStore, "Cleo")
+	})
 }
